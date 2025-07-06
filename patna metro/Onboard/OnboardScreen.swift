a07f208fa3c2b5bab69119272d42222453a509ae
shashwat283 <shashwatrajwanshi41@gmail.com>
2025-07-06 22:37:21 +0530

    added onboard screen + correct the route count

---
+++ patna metro/Onboard/OnboardScreen.swift
@@ -0,0 +1,18 @@
+//
+//  OnboardScreen.swift
+//  patna metro
+//
+//  Created by shashwat singh on 06/07/25.
+//
+
+import SwiftUI
+
+struct OnboardScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OnboardScreen()
+}
